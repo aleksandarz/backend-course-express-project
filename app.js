af28f7f887e1356c7967a7099d4750eb2b2a81a9
aleksandarz <zaleksandar888@gmail.com>
2026-04-03 12:05:41 +0200

    feat(api): add POST /api/v1/tours endpoint

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as fs from "fs";
 
 const app = express();
+app.use(express.json());
+
 const __dirname = import.meta.dirname;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
@@ -13,6 +15,24 @@ app.get("/api/v1/tours", (req, res) => {
     data: {
       tours: tours
     },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+
+  let newId = tours[tours.length - 1].id + 1;
+  let newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), "utf-8", (err) => {
+    if (err) return console.log(err);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    });
   });
 });
 
