90818211724d15bb29f37012364dc38d59239c0e
aleksandarz <zaleksandar888@gmail.com>
2026-04-16 10:00:13 +0200

    feat: connect MongoDB with Mongoose and create initial Tour model

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
+import mongoose from "mongoose";
 import "dotenv/config";
 import app from "./app.js";
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+const conn = await mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+if (conn) {
+  console.log("Connected successfully");
+}
+
+const tourSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "A tour name is required"], unique: true },
+  rating: { type: Number, required: false, default: 4.5 },
+  price: { type: Number, required: [true, "A tour price is required"] },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+const doc1 = await testTour.save();
+console.log(doc1);
 
 const PORT = process.env.PORT || 8080;
 const HOSTNAME = process.env.HOSTNAME || "localhost";
