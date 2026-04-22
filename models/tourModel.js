d917691a1f40583f774edc180f59ff520f82d7bf
aleksandarz <zaleksandar888@gmail.com>
2026-04-22 12:03:16 +0200

    refactor: update tour schema with additional fields and defaults

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,9 +1,57 @@
 import mongoose from "mongoose";
 
 const tourSchema = new mongoose.Schema({
-  name: { type: String, required: [true, "A tour name is required"], unique: true },
-  rating: { type: Number, required: false, default: 4.5 },
-  price: { type: Number, required: [true, "A tour price is required"] },
+  name: {
+    type: String,
+    required: [true, "A tour name is required"],
+    unique: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour duration is required"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour difficulty is required"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour price is required"],
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour summary is required"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
