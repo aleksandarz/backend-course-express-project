5a996f6c66f0601be81490bbc4489cff5c8edb0c
aleksandarz <zaleksandar888@gmail.com>
2026-04-27 11:46:15 +0200

    feat: add pagination to tours endpoint using page and limit query params

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,6 +27,18 @@ export const getAllTours = async (req, res) => {
       query = query.select("-__v");
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error("This page doesn't exist!");
+      }
+    }
+
     const tours = await query;
 
     res.status(200).json({
@@ -39,7 +51,7 @@ export const getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err,
+      message: err.message,
     });
   }
 }
