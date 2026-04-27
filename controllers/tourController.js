fe37ac68fb49cde4d2c2bd8679980431b6d50ea1
aleksandarz <zaleksandar888@gmail.com>
2026-04-27 12:05:04 +0200

    feat: add middleware to preset query for top 5 tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 import Tour from "../models/tourModel.js";
 
+export const aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+}
+
 export const getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query }
