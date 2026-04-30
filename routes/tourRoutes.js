0b1756b0874eb5917c64cdd51b455b7440362c63
aleksandarz <zaleksandar888@gmail.com>
2026-04-30 11:33:41 +0200

    feat: implement monthly tour plan with aggregation (unwind, match, group, sort)

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ import {
   createNewTour,
   deleteTour,
   getAllTours,
+  getMonthlyPlan,
   getSingleTour,
   getTourStats,
   updateTourWithPatch,
@@ -14,6 +15,8 @@ const router = express.Router();
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 
 router.route("/tour-stats").get(getTourStats);
+
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/").get(getAllTours).post(createNewTour);
 
