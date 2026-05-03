104a94536527be42b31e4b1c3485d3d254cce225
aleksandarz <zaleksandar888@gmail.com>
2026-05-03 12:50:45 +0200

    feat: add mongoose query middleware for slug generation and query filtering

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema({
     select: false,
   },
   startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true },
@@ -64,15 +68,23 @@ tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
 
-tourSchema.pre("save", function (next) {
+tourSchema.pre("save", async function() {
   this.slug = slugify(this.name, { lower: true });
-  next();
 });
 
-// tourSchema.post("save", function (doc, next) {
+// tourSchema.post("save", async function (doc) {
 //   console.log(doc);
-//   next();
 // });
+
+tourSchema.pre(/^find/, async function () {
+  this.where({ secretTour: { $ne: true } });
+  this.start = Date.now();
+});
+
+tourSchema.post(/^find/, async function (docs) {
+  console.log(`Query took ${Date.now() - this.start}ms`);
+  console.log(docs);
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
