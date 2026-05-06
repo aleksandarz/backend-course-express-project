906203713f8c0af3a7ad042bb0add63e2c5872c7
aleksandarz <zaleksandar888@gmail.com>
2026-05-06 10:47:18 +0200

    feat: add custom middleware for non existing routes

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getSingleTour);
-// app.post("/api/v1/tours", createNewTour);
-// app.patch("/api/v1/tours/:id", updateTourWithPatch);
-// app.delete("/api/v1/tours/:id", deleteTour);
-
-// Refactored routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 export default app;
